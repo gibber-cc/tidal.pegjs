3f6935115e8996efe6a47bd2154c741907c2c54d
mariana0pachon <mpachonpuentes@wpi.edu>
2019-04-27 23:21:54 -0400

    Added documentation for flatten code and tests

--- test/test-flatten.js
+++ test/test-flatten.js
@@ -9,32 +9,42 @@ const assert = require( 'assert');
 
 
 /**
- * [flatten description]
- * @param  {[type]} group [description]
- * @return {[type]}       [description]
+ * Flattens objects that resulted from parsing Tidal patterns
+ * @param  {Object} group the nested (or not nested) object to be flattened
+ * @return {Object}       the flattened object with correct fractions in a cycle
  */
 function flatten(group){
 
-  let currentPosition = new Fraction(0)
+  let currentPosition = new Fraction(0) // Start at 0 and increase every iteration
   const flattened = {}
 
+  /**
+   * Recursive inner function to add proper fractions as keys to the flattened object
+   * @param  {Object}   group    the group or subgroup to be flattened
+   * @param  {Fraction} dur      the duration of each subgroup in the group
+   */
   function calc( group, dur = new Fraction( 1, Object.keys(group).length - 1 ) ){
+
     for( let key in group ) {
-      if( key === 'type' ) continue
+
+      if( key === 'type' ) continue // No type specification in the flattened object
+
       const step = group[ key ]
-      if( step.type === 'group' ) {
+
+      if( step.type === 'group' ) { // Nested step so do a recursive call
         const groupDur = new Fraction( 1, Object.keys( step ).length - 1 )
         calc( step, dur.mul( groupDur ) )
-      }else{
+      }
+      else{ // Step is flat so add to global flattened const
         flattened[ currentPosition.toFraction( false ) ] = step
         currentPosition = currentPosition.add( dur )
       }
     }
   }
 
-  calc(group)
+  calc(group) // Call to the inner function
 
-  return flattened
+  return flattened // Final flattened object
 
 }
 
