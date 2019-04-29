9cae9800785804b28027b5001bcd08eeb8c347e8
mariana0pachon <mpachonpuentes@wpi.edu>
2019-04-28 21:29:06 -0400

    Tests for rest pass

--- test/test-rest.js
+++ test/test-rest.js
@@ -1,44 +1,31 @@
-// /* test-rest.js
-//  *
-//  * A test for rests in Tidal
-//  *
-//  */
-//
-// const peg    = require( 'pegjs' )
-// const fs     = require( 'fs' )
-// const assert = require( 'assert')
-//
-// const grammar = fs.readFileSync( __dirname + '/../tidal.pegjs', { encoding:'utf-8' })
-// const parser  = peg.generate( grammar )
-//
-// describe( 'Testing rests.', () => {
-//   /*
-//    * "0 ~ 2 ~"
-//    *
-//    * ->
-//    *
-//    *  [
-//    *    { type:'number', value:0 },
-//    *    { type:'rest' },
-//    *    { type:'number', value:2 },
-//    *    { type:'rest' }
-//    *    type:'group'
-//    *  ]
-//    *
-//    */
-//
-//   it( 'should generate a rest object when parsing a ~', () => {
-//     const group = [
-//       { type:'number', value:0 },
-//       { type:'rest' },
-//       { type:'number', value:2 },
-//       { type:'rest' },
-//     ]
-//     group.type  = 'group'
-//
-//     const result = parser.parse( '0 ~ 2 ~' )
-//
-//     assert.deepEqual( group, result )
-//   })
-//
-// })
+/* test-rest.js
+ *
+ * A test for rests in Tidal
+ *
+ */
+
+const peg    = require( 'pegjs' )
+const fs     = require( 'fs' )
+const assert = require( 'assert')
+
+const grammar = fs.readFileSync( __dirname + '/../tidal.pegjs', { encoding:'utf-8' })
+const parser  = peg.generate( grammar )
+
+describe( 'Testing rests.', () => {
+
+
+  it( 'should generate a rest object when parsing a ~', () => {
+    const expected = {
+      '0': { type:'number', value:0 },
+      '1/4': { type:'rest' },
+      '1/2': { type:'number', value:2 },
+      '3/4': { type:'rest' },
+    }
+    expected.type  = 'group'
+
+    const result = parser.parse( '0 ~ 2 ~' )
+
+    assert.deepEqual( result, expected )
+  })
+
+})
