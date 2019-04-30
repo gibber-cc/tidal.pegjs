4b825ffadfca94f1d28ec619b28ad934e58e2295
mariana0pachon <mpachonpuentes@wpi.edu>
2019-04-30 19:43:33 -0400

    Added a test combining repeat and euclid

--- test/test-repeat.js
+++ test/test-repeat.js
@@ -48,4 +48,38 @@ describe( "Testing repeats with '*'", () => {
 
   });
 
+  it('test euclid and repeat', () => {
+
+    const expected = {
+      '0': {type: 'number', value: 0},
+      '1/3':{
+        '0':{
+          '0': {type: 'number', value:2},
+          '1/2': {type: 'number', value: 3},
+          type: 'group'
+        },
+        '1/2': {
+          '0': {type: 'number', value: 2},
+          '1/3': {
+            value: {type: 'number', value: 4},
+            soundNum: {type: 'number', value: 3},
+            steps: {type: 'number', value: 8},
+            rotateStep: {type: 'number', value: 9},
+            type: 'euclid'
+          },
+          '2/3': {
+            type: 'repeat',
+            operator: '*',
+            repeatValue: {type: 'number', value: 2},
+            value: {type: 'number', value:7}
+          }
+        }
+      },
+      '2/3': {type: 'number', value: 5},
+      type: 'group'
+    }
+
+    const result = parser.parse('0 [[2 3] [2 4(3,8,9) 7*2]] 5');
+  });
+
 })
