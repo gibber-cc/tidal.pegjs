c0c3381c186e6caaeab89ed15f6266cad4124cab
mariana0pachon <mpachonpuentes@wpi.edu>
2019-04-30 18:20:07 -0400

    Fixed module exports

--- src/flatten.js
+++ src/flatten.js
@@ -15,69 +15,68 @@ let parseFile = require( './parseToObject.js' );
  */
 
 module.exports.flatten =
+  function flatten(group){
 
-    function flatten(group){
+    let currentPosition = new Fraction(0) // Start at 0 and increase every iteration
+    const flattened = {}
 
-      let currentPosition = new Fraction(0) // Start at 0 and increase every iteration
-      const flattened = {}
+    /**
+     * Recursive inner function to add proper fractions as keys to the flattened object
+     * @param  {Object}   group    the group or subgroup to be flattened
+     * @param  {Fraction} dur      the duration of each subgroup in the group
+     */
+    function calc( group, dur = new Fraction( 1, Object.keys(group).length - 1 ) ){
 
-      /**
-       * Recursive inner function to add proper fractions as keys to the flattened object
-       * @param  {Object}   group    the group or subgroup to be flattened
-       * @param  {Fraction} dur      the duration of each subgroup in the group
-       */
-      function calc( group, dur = new Fraction( 1, Object.keys(group).length - 1 ) ){
+      if (group.type === 'repeat'){
 
-        if (group.type === 'repeat'){
+        if (group.value.type === 'group'){
 
-          if (group.value.type === 'group'){
+          let length = (Object.keys(group.value).length - 1) * group.repeatValue.value;
 
-            let length = (Object.keys(group.value).length - 1) * group.repeatValue.value;
+          let groupDur = new Fraction(1, length)
 
-            let groupDur = new Fraction(1, length)
+          let currentPosition = new Fraction(0);
 
-            let currentPosition = new Fraction(0);
+            for (let i=0; i<group.repeatValue.value; i++){
+              for ( let key in group.value ){
 
-              for (let i=0; i<group.repeatValue.value; i++){
-                for ( let key in group.value ){
+                if (key === 'type') continue
 
-                  if (key === 'type') continue
+                const step = group.value[key]
 
-                  const step = group.value[key]
-
-                  flattened[currentPosition.toFraction(false)] = step;
-                  currentPosition = currentPosition.add(groupDur);
-                }
+                flattened[currentPosition.toFraction(false)] = step;
+                currentPosition = currentPosition.add(groupDur);
               }
-          }
-          else {
-            flattened[currentPosition.toFraction(false)] = group.value;
-          }
-
-          flattened.type = 'group';
-
-          return;
-
+            }
+        }
+        else {
+          flattened[currentPosition.toFraction(false)] = group.value;
         }
 
-        for( let key in group ) {
+        flattened.type = 'group';
 
-          if( key === 'type' ) continue // No type specification in the flattened object
+        return;
 
-          const step = group[ key ]
-
-          if( step.type === 'group' ) { // Nested step so do a recursive call
-            const groupDur = new Fraction( 1, Object.keys( step ).length - 1 )
-            calc( step, dur.mul( groupDur ) )
-          }
-          else{ // Step is flat so add to global flattened const
-            flattened[ currentPosition.toFraction( false ) ] = step
-            currentPosition = currentPosition.add( dur )
-          }
-        }
       }
 
-      calc(group) // Call to the inner function
+      for( let key in group ) {
 
-      return flattened // Final flattened object
+        if( key === 'type' ) continue // No type specification in the flattened object
+
+        const step = group[ key ]
+
+        if( step.type === 'group' ) { // Nested step so do a recursive call
+          const groupDur = new Fraction( 1, Object.keys( step ).length - 1 )
+          calc( step, dur.mul( groupDur ) )
+        }
+        else{ // Step is flat so add to global flattened const
+          flattened[ currentPosition.toFraction( false ) ] = step
+          currentPosition = currentPosition.add( dur )
+        }
+      }
     }
+
+    calc(group) // Call to the inner function
+
+    return flattened // Final flattened object
+  }
--- src/parseToObject.js
+++ src/parseToObject.js
@@ -16,15 +16,15 @@ var Fraction = require('fraction.js');
  * @return  {Object}             the parsed object
  */
 module.exports.parseToObject =
-    function parseToObject(body, start = new Fraction(0), end = new Fraction(1)){
+  function parseToObject(body, start = new Fraction(0), end = new Fraction(1)){
 
-      let cycleBody = {};
-      let cycleSection = start;
-      let frac = new Fraction(end/body.length);
+    let cycleBody = {};
+    let cycleSection = start;
+    let frac = new Fraction(end/body.length);
 
-      for( let i = 0; i < body.length; i++ ) {
-        cycleBody[cycleSection.toFraction(false)] = body[i];
-        cycleSection = new Fraction(cycleSection).add(frac);
-      }
-      return cycleBody;
+    for( let i = 0; i < body.length; i++ ) {
+      cycleBody[cycleSection.toFraction(false)] = body[i];
+      cycleSection = new Fraction(cycleSection).add(frac);
     }
+    return cycleBody;
+  }
