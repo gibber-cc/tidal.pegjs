8eb03fef88e207f1359013ac3d103e5518728c73
charlieroberts <charlie@charlie-roberts.com>
2019-06-08 12:20:15 -0700

    file for pattern object

---
+++ src/pattern.js
@@ -0,0 +1,54 @@
+const parse = require('../dist/tidal.js').parse
+const query = require('./queryArc.js' ).queryArc
+const Fraction = require( 'fraction.js' )
+
+/* The Pattern object is used to parse a pattern
+ * a single time and then query it repeatedly, assuming
+ * different start and end times for each query. A priority
+ * queue is used to sort the events... 
+*/
+const Pattern = patternString => {
+  if( typeof patternString !== 'string' )
+    throw 'You must provide a string to generate the pattern from'
+
+  let __data
+  try{
+    __data = parse( patternString )
+  }catch( e ) {
+    throw `We were unable to parse the pattern ${patternString}. Error: ${e.toString()}`
+  }
+
+  const ptrn = {
+    __rawString: patternString,
+    __data,
+
+    events: null,
+
+    __sort( a,b ) { return a.arc.start.compare( b.arc.start ) },
+    query( start, duration ) {
+      if( typeof start !== 'object' ) start = Fraction( start )
+      if( typeof duration !== 'object' ) duration = Fraction( duration )
+
+      ptrn.events = query( ptrn.__data, start, duration )
+        .sort( ptrn.__sort )
+
+      return ptrn.events
+    },
+
+    print() {
+      if( ptrn.events !== null ) {
+        ptrn.events.forEach( v => 
+          console.log( 
+            `${v.arc.start.toFraction()} - ${v.arc.end.toFraction()}: [ ${v.value.toString()} ]` 
+          ) 
+        )
+      }else{
+        console.log( 'No events have been generated from the pattern; have you queried it yet?' )
+      }
+    }
+  }
+
+  return ptrn
+}
+
+module.exports = Pattern
