b122192d5bf777fb28ef167d53f2ab6048e85acb
charlieroberts <charlie@charlie-roberts.com>
2019-06-09 11:29:55 -0700

    fix for onestep

--- src/queryArc.js
+++ src/queryArc.js
@@ -238,7 +238,7 @@ const handlers = {
 
       state.push({ 
         arc:Arc( phase, phase.add( duration ) ), 
-        value:group.values[ group.count ].value 
+        value:group.values[ group.count % group.values.length ].value 
       })
     })
 
